080dd8562b650668804864633ee8e714ba2e9cb2
Giorgi <giorgirokhadze@gmail.com>
2016-04-17 19:20:07 +0400

    All retested! :tada:

--- src/me/giorgirokhadze/cryptography/test/DecryptMain.java
+++ src/me/giorgirokhadze/cryptography/test/DecryptMain.java
@@ -13,6 +13,13 @@ public class DecryptMain {
         };
         byte[] key = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
         System.out.println("Finish: ".concat(new String(AES.decrypt(encrypted, key))));
+        encrypted = new byte[]{
+                43, 106, 124, 99, 9, 31, 23, 10,
+                121, 86, 60, 109, 52, 62, -80, 44,
+                81, 23, -99, -61, 57, -126, 86, 44,
+                25, -33, -123, -3, 61, 69, -87, -121
+        };
+        System.out.println("Finish: ".concat(new String(AES.decrypt(encrypted, key))));
     }
 
 }
--- src/me/giorgirokhadze/cryptography/test/EncryptMain.java
+++ src/me/giorgirokhadze/cryptography/test/EncryptMain.java
@@ -11,6 +11,8 @@ class EncryptMain {
         String test = "asdfghjkasdfghjk";
         byte[] key = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
         System.out.println("Finish: ".concat(Arrays.toString(AES.encrypt(test.getBytes(), key))));
+        test = "123!@#qweQWEasdASD";
+        System.out.println("Finish: ".concat(Arrays.toString(AES.encrypt(test.getBytes(), key))));
     }
 
 }
